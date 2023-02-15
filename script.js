29b10754c590444ffd3a250993fa6c31fea27e0d
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2023-02-15 11:03:09 -0500

    update css, add new js to retrieve and match jobs-data, update html

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-const jobsList = document.getElementById("jobs-list");
-  
-  jobs.forEach((job) => {
-    const li = document.createElement("li");
-    const title = document.createElement("h3");
-    const company = document.createElement("p");
-    const location = document.createElement("p");
-    const description = document.createElement("p");
-    const applyLink = document.createElement("a");
-  
-    title.textContent = job.title;
-    company.textContent = job.company;
-    location.textContent = job.location;
-    description.textContent = job.description;
-    applyLink.textContent = "Apply now";
-    applyLink.href = job.applyLink;
-  
-    li.appendChild(title);
-    li.appendChild(company);
-    li.appendChild(location);
-    li.appendChild(description);
-    li.appendChild(applyLink);
-  
-    jobsList.appendChild(li);
+// Get a reference to the job list container in the HTML
+const jobList = document.getElementById("job-list");
+
+// Fetch the job data from the JSON file
+fetch("jobs-data.json")
+  .then(response => response.json())
+  .then(data => {
+    // Iterate through each job in the data
+    data.featuredJobs.forEach(job => {
+      // Create a new job listing element
+      const jobListing = document.createElement("div");
+      jobListing.classList.add("job");
+
+      // Populate the job listing element with the job data
+      jobListing.innerHTML = `
+        <h3 class="job-title">${job.title}</h3>
+        <h4 class="job-company">${job.company}</h4>
+        <p class="job-location">${job.location}</p>
+        <p class="job-salary">${job.salary}</p>
+        <p class="job-description">${job.description}</p>
+        <ul class="job-requirements">
+          ${job.requirements.map(req => `<li>${req}</li>`).join("")}
+        </ul>
+        <a class="job-apply-btn" href="${job.applyUrl}" target="_blank">Apply Now</a>
+      `;
+
+      // Add the job listing element to the job list container
+      jobList.appendChild(jobListing);
+    });
+  })
+  .catch(error => {
+    console.error("Error fetching job data:", error);
   });
-  
